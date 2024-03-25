83662ea308b8bda6b47613ea2c1437e7df642c42
Julia Costa <juliacosta232005@gmail.com>
2024-03-25 22:30:43 +0000

    Added GaloDaVelha project

---
+++ GalodaVelha/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GalodaVelha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

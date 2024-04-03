7eb3e0af0d898d61e02f66c42e182be1f0cc3562
Julia Costa <juliacosta232005@gmail.com>
2024-04-03 14:47:58 +0100

    Created temporary ColoredText() Method for test purposes

--- GalodaVelha/Program.cs
+++ GalodaVelha/Program.cs
@@ -6,7 +6,16 @@ namespace GalodaVelha
     {
         static void Main(string[] args)
         {
-            
+            string s = "Test Print";
+            ColoredText(s, ConsoleColor.Red);
+            ColoredText(s, ConsoleColor.Green);
+            ColoredText(s, ConsoleColor.Blue);
+        }
+        static private void ColoredText(string str, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(str);
+            Console.ResetColor();
         }
     }
 }

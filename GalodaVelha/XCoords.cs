a47e6f58fcee596a897d866d0bab8011e205b17e
Julia Costa <juliacosta232005@gmail.com>
2024-04-04 20:31:09 +0100

    Add XCoords enum to decode board coordinates

---
+++ GalodaVelha/XCoords.cs
@@ -0,0 +1,14 @@
+namespace GalodaVelha
+{
+    /// <summary>
+    /// Converts letter inputs into integer numbers to use as coordinates for
+    /// the game board.
+    /// </summary>
+    public enum XCoords
+    {
+        A = 1,
+        B = 2,
+        C = 3,
+        D = 4
+    }
+}

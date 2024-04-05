458788d2bd6c8b19399e4344d393099b47886d14
Julia Costa <juliacosta232005@gmail.com>
2024-04-05 01:30:32 +0100

    Add AskForCoord(), CheckCoordInRange() and CoordIsEmpty() methods to GameBoardclass

--- GalodaVelha/GameBoard.cs
+++ GalodaVelha/GameBoard.cs
@@ -34,6 +34,71 @@ namespace GalodaVelha
 
             return playerChoice;
         }
+        private int[] AskForCoords()
+        {
+            int[] coords = new int[2];
+
+            Console.Write($"{player} choose a coordinate to place:\n>");
+            string coordsInput = Console.ReadLine();
+            Console.WriteLine();
+
+            string coordX = coordsInput[0].ToString();
+            string coordY = coordsInput[1].ToString();
+
+            if (coordsInput.Length == 2)
+            {
+                if (CheckCoordInRange(coordX) & int.Parse(coordY) <= 4)
+                {
+                    coords[0] = (int)Enum.Parse(typeof(XCoords),coordX) - 1;
+                    coords[1] = int.Parse(coordY) - 1;
+
+                    if (!CoordIsEmpty(coords))
+                    {
+                        Console.WriteLine("Invalid coord, already occupied!");
+                        Console.WriteLine("Please try again.\n");
+                        coords = AskForCoords();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid coord, out of board range!");
+                    Console.WriteLine("Please try again.\n");
+                    coords = AskForCoords();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid coord, more than 2 values given!");
+                Console.WriteLine("Please try again.\n");
+                coords = AskForCoords();
+            }
+
+            return coords;
+        }
+        private bool CheckCoordInRange(string coord)
+        {
+            bool presence = false;
+
+            foreach (string letter in Enum.GetNames(typeof(XCoords)))
+            {
+                if (coord == letter)
+                {
+                    presence = true;
+                }
+            }
+            return presence;
+        }
+        
+        private bool CoordIsEmpty(int[] coord)
+        {
+            bool check = false;
+
+            if (board[coord[0],coord[1]] == " ")
+            {
+                check = true;
+            }
+            return check;
+        }
        
     }
 }

1aa117056fc31ebc6da7e2bc44f3d913821e4c08
Julia Costa <juliacosta232005@gmail.com>
2024-04-05 03:18:00 +0100

    Add documentation to GameBoard class

--- GalodaVelha/GameBoard.cs
+++ GalodaVelha/GameBoard.cs
@@ -5,6 +5,10 @@ using System.Threading.Tasks;
 
 namespace GalodaVelha
 {
+    /// <summary>
+    /// Class that controls the game board and how the players will interact
+    /// with it.
+    /// </summary>
     public class GameBoard
     {
         string[,] board;
@@ -18,47 +22,82 @@ namespace GalodaVelha
                                           {" "," "," "," "},
                                           {" "," "," "," "}};
         }
+        /// <summary>
+        /// Call AskForPiece() and AskForCoords() methods and insert the
+        /// results to the board matrix.
+        /// </summary>
         public void AskForInputs()
         {
-            placePiece = AskForPiece();
+            //Get what piece the adversary wants to be played
+            placePiece = AskForPiece(); 
+            //Get the coordinates where the player wants to put the piece
             placeCoords = AskForCoords();
+            //Insert the piece into the board matrix with the specefied coords
             board[placeCoords[0],placeCoords[1]] = placePiece.GetName();
         }
+        /// <summary>
+        /// Ask the adversary what piece they want to give to the player.
+        /// </summary>
+        /// <returns>Piece class instance with the specified traits.</returns>
         private Piece AskForPiece()
         {
-            Console.Write($"{player} choose a piece:\n>");
+            //Ask the adversary for a piece
+            Console.Write($"{player} choose a piece:\n> ");
             string pieceCode = Console.ReadLine();
             Console.WriteLine();
 
+            //Instanciate a new piece with the code given by the adversary
             Piece playerChoice = new Piece(pieceCode);
 
+            //Check if the choosen piece is valid
             if (!playerChoice.validity)
             {
+                //If false call AskForPiece() again
                 playerChoice = AskForPiece();
             }
 
+            //Return the specified piece
             return playerChoice;
         }
+        /// <summary>
+        /// Ask the player for the coordiantes where he wants to insert the 
+        /// given piece.
+        /// </summary>
+        /// <returns>Array of integers with the XY coordinates.</returns>
         private int[] AskForCoords()
         {
+            //Initialize an integer array to store the coords
             int[] coords = new int[2];
 
+            //Ask the player for the coordinates
             Console.Write($"{player} choose a coordinate to place:\n>");
             string coordsInput = Console.ReadLine();
             Console.WriteLine();
 
-            string coordX = coordsInput[0].ToString();
+            //Split the string into two char values and convert
+            //them to strings
+            string coordX = coordsInput[0].ToString(); 
             string coordY = coordsInput[1].ToString();
 
+            //Check if the player input has the right length
+            //It can only accept two values
             if (coordsInput.Length == 2)
             {
+                //If true check if the given values are within the values of
+                //the 4x4 board matrix
                 if (CheckCoordInRange(coordX) & int.Parse(coordY) <= 4)
                 {
+                    //If true convert the values into integers and insert
+                    //them to the array
                     coords[0] = (int)Enum.Parse(typeof(XCoords),coordX) - 1;
                     coords[1] = int.Parse(coordY) - 1;
 
+                    //Check if the specified coords are empty inside the board
+                    //matrix
                     if (!CoordIsEmpty(coords))
                     {
+                        //If false print a message to the console and
+                        //call AskForCoords() again
                         Console.WriteLine("Invalid coord, already occupied!");
                         Console.WriteLine("Please try again.\n");
                         coords = AskForCoords();
@@ -66,6 +105,8 @@ namespace GalodaVelha
                 }
                 else
                 {
+                    //If false print a message to the console and
+                    //call AskForCoords() again
                     Console.WriteLine("Invalid coord, out of board range!");
                     Console.WriteLine("Please try again.\n");
                     coords = AskForCoords();
@@ -73,36 +114,63 @@ namespace GalodaVelha
             }
             else
             {
+                //If false print a message to the console and
+                //call AskForCoords() again
                 Console.WriteLine("Invalid coord, more than 2 values given!");
                 Console.WriteLine("Please try again.\n");
                 coords = AskForCoords();
             }
 
+            //Return the integer array with the XY coordinates
             return coords;
         }
+        /// <summary>
+        /// Check if the given X coordinate is in range of the game board.
+        /// </summary>
+        /// <param name="coord">Type string coordinate.</param>
+        /// <returns>True if the coordinate is in range and false
+        /// if otherwise.</returns>
         private bool CheckCoordInRange(string coord)
         {
-            bool presence = false;
-
-            foreach (string letter in Enum.GetNames(typeof(XCoords)))
-            {
-                if (coord == letter)
-                {
-                    presence = true;
-                }
-            }
-            return presence;
-        }
-        private bool CoordIsEmpty(int[] coord)
-        {
+            //Intialize a bool type variable as false
             bool check = false;
 
-            if (board[coord[0],coord[1]] == " ")
+            //Go for each value of the XCoord enum and get their name
+            foreach (string letter in Enum.GetNames(typeof(XCoords)))
             {
-                check = true;
+                //Check if the name is equal to the coord
+                if (coord == letter)
+                {
+                    //If true change check variable to true
+                    check = true;
+                }
             }
+            //Return bool variable
             return check;
         }
+        /// <summary>
+        /// Check if the given coordinate in the board matrix is empty.
+        /// </summary>
+        /// <param name="coord">Integer array with the XY coordinates.</param>
+        /// <returns>True if the given coordinate is empty and false 
+        /// if it has a value.</returns>
+        private bool CoordIsEmpty(int[] coord)
+        {
+            //Initialize bool type varaible as false
+            bool check = false;
+
+            //Check if the given position of the matrix is empty
+            if (board[coord[0],coord[1]] == " ")
+            {
+                //If true change check variable to true
+                check = true;
+            }
+            //Return bool type variable
+            return check;
+        }
+        /// <summary>
+        /// Print the game board to the console.
+        /// </summary>
         public void PrintBoard()
         {
             ColoredText("     A   B   C   D\n",ConsoleColor.Yellow);
@@ -124,10 +192,18 @@ namespace GalodaVelha
             $" {board[2,3]} | {board[3,3]} |");
             Console.WriteLine("   +---+---+---+---+");
         }
+        /// <summary>
+        /// Print colored text to the console.
+        /// </summary>
+        /// <param name="str">String to print.</param>
+        /// <param name="color">Color to print in.</param>
         private void ColoredText(string str, ConsoleColor color)
         {
+            //Change console foreground color
             Console.ForegroundColor = color;
+            //Print given string to the console
             Console.Write(str);
+            //Reset console color
             Console.ResetColor();
         }
 

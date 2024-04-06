e74c20c6ec70b9a665c14a039d6c6655b3561d0a
Julia Costa <juliacosta232005@gmail.com>
2024-04-06 10:50:40 +0100

    Clean up Main

--- GalodaVelha/Program.cs
+++ GalodaVelha/Program.cs
@@ -10,24 +10,6 @@ namespace GalodaVelha
         {
             Console.OutputEncoding = Encoding.UTF8;
 
-            char a = '\u25A0';
-            char b = '\u25A1';
-            char c = '\u25AA';
-            char d = '\u25AB';
-            char e = '\u25CF';
-            char f = '\u25CB';
-            char g = '\u2022';
-            char h = '\u25E6';
-
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
-            Console.WriteLine(d);
-            Console.WriteLine(e);
-            Console.WriteLine(f);
-            Console.WriteLine(g);
-            Console.WriteLine(h);
-
             GameManager Game = new GameManager();
 
             Game.GameStart();
